896745fe85f559e0894a72ee86f610d52004ded0
Martin Rejstrom <rejstrom@gmail.com>
2020-02-16 20:37:44 +0200

    Updates

---
+++ precache-manifest.81e8b468427c1526c684c30d076e8e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e80540f5c4bcf2dd4f6fa7e5ce13efe",
+    "url": "/spms/index.html"
+  },
+  {
+    "revision": "f751574abf9feb751904",
+    "url": "/spms/static/css/main.42ebfae4.chunk.css"
+  },
+  {
+    "revision": "3c9555ba62a6291f8233",
+    "url": "/spms/static/js/2.37952f16.chunk.js"
+  },
+  {
+    "revision": "f751574abf9feb751904",
+    "url": "/spms/static/js/main.138deb37.chunk.js"
+  },
+  {
+    "revision": "bc4e1fe999dd765040fa",
+    "url": "/spms/static/js/runtime-main.d86af622.js"
+  },
+  {
+    "revision": "135d4e43ea83702b6174ebebbe63faca",
+    "url": "/spms/static/media/arrow_down.135d4e43.svg"
+  }
+]);
